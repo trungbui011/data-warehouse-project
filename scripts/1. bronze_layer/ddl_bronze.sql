e7682c6bec45e0e0af0740918752de086c3334e2
Trung Bui <61272192+trungbui011@users.noreply.github.com>
2026-07-06 14:36:24 +0700

    Update ddl_bronze.sql

--- scripts/1. bronze_layer/ddl_bronze.sql
+++ scripts/1. bronze_layer/ddl_bronze.sql
@@ -7,70 +7,82 @@ TẠO CÁC BẢNG CHO LỚP BRONZE
 
 */
 
-IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.crm_cust_info
-CREATE TABLE bronze.crm_cust_info (
-	cst_id NVARCHAR(50),
-	cst_key CHAR(10),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_marital_status NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
-	cst_create_date DATE
-);
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.crm_cust_info (
+		cst_id NVARCHAR(50),
+		cst_key CHAR(10),
+		cst_firstname NVARCHAR(50),
+		cst_lastname NVARCHAR(50),
+		cst_marital_status NVARCHAR(50),
+		cst_gndr NVARCHAR(50),
+		cst_create_date DATE,
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
 
-IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.crm_prd_info
-CREATE TABLE bronze.crm_prd_info (
-	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE
-);
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.crm_prd_info (
+		prd_id INT,
+		prd_key NVARCHAR(50),
+		prd_nm NVARCHAR(50),
+		prd_cost INT,
+		prd_line NVARCHAR(50),
+		prd_start_dt DATE,
+		prd_end_dt DATE,
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
 
-IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.crm_sales_details	
-CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id NVARCHAR(50)),
-	sls_order_dt NVARCHAR(50),
-	sls_ship_dt NVARCHAR(50),
-	sls_due_dt NVARCHAR(50),
-	sls_sales NVARCHAR(50),
-	sls_quantity NVARCHAR(50),
-	sls_price NVARCHAR(50)
-);
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.crm_sales_details (
+		sls_ord_num NVARCHAR(50),
+		sls_prd_key NVARCHAR(50),
+		sls_cust_id NVARCHAR(50)),
+		sls_order_dt NVARCHAR(50),
+		sls_ship_dt NVARCHAR(50),
+		sls_due_dt NVARCHAR(50),
+		sls_sales NVARCHAR(50),
+		sls_quantity NVARCHAR(50),
+		sls_price NVARCHAR(50),
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
 
-IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.erp_cust_az12	
-CREATE TABLE bronze.erp_cust_az12 (
-	cid NVARCHAR(50),
-	bdate DATE,
-	gen NVARCHAR(50)
-);
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.erp_cust_az12 (
+		cid NVARCHAR(50),
+		bdate DATE,
+		gen NVARCHAR(50),
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
 
-IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.erp_loc_a101	
-CREATE TABLE bronze.erp_loc_a101(
-	cid NVARCHAR(50),
-	cntry NVARCHAR(50)
-);
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.erp_loc_a101(
+		cid NVARCHAR(50),
+		cntry NVARCHAR(50),
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
 
-IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
-DROP TABLE bronze.erp_px_cat_g1v2
-CREATE TABLE bronze.erp_px_cat_g1v2(
-	id NVARCHAR(50),
-	cat NVARCHAR(50),
-	subcat NVARCHAR(50),
-	maintenance NVARCHAR(50)
-);
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NULL -- Nếu bảng không tồn tại -> Trả về rỗng (NULL); Nếu bảng tồn tại (không rỗng) -> Drop table
+BEGIN
+	CREATE TABLE bronze.erp_px_cat_g1v2(
+		id NVARCHAR(50),
+		cat NVARCHAR(50),
+		subcat NVARCHAR(50),
+		maintenance NVARCHAR(50),
+		dwh_create_date DATETIME DEFAULT GETDATE()
+	);
+END
 GO
